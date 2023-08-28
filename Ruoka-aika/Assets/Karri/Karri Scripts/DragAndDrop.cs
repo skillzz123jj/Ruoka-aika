a87ce4f02217ce41892d242b31d89170f8cab4bb
Karri Sivén <meatballgod89@gmail.com>
2023-08-28 12:02:35 +0300

    Liikutus Script
    Ruoat liikkuu

---
+++ Ruoka-aika/Assets/Karri/Karri Scripts/DragAndDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool isDragging;
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDragging)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+    }
+}
