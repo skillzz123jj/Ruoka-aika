e6a8ef06bbf7d3fc7324111599c21c82d90b2e67
skillzz123jj <turpeinenmila6@gmail.com>
2023-09-01 17:43:14 +0300

    Updated main menu again

--- Ruoka-aika/Assets/Mila/Scripts/MainMenu.cs
+++ Ruoka-aika/Assets/Mila/Scripts/MainMenu.cs
@@ -1,14 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button easyButton;
+    public Button normalButton;
+    public Button hardButton;
+
+    public Sprite easyHighlightSprite;
+    public Sprite normalHighlightSprite;
+    public Sprite hardHighlightSprite;
+
+    public Sprite easyDefaultSprite;
+    public Sprite normalDefaultSprite;
+    public Sprite hardDefaultSprite;
+
     //These are for buttons
     public void StartGame(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+    
+    private void Update()
+    {
+        if (Difficulty.difficulty.easy)
+        {
+            easyButton.image.sprite = easyHighlightSprite;
+            //normalButton.image.sprite = normalDefaultSprite;
+            //hardButton.image.sprite = hardDefaultSprite;
+
+        }
+        if (Difficulty.difficulty.normal)
+        {
+            normalButton.image.sprite = normalHighlightSprite;
+            //easyButton.image.sprite = easyDefaultSprite;
+            //hardButton.image.sprite = hardDefaultSprite;
+
+        }
+        if (Difficulty.difficulty.hard)
+        {
+            hardButton.image.sprite = hardHighlightSprite;
+            //easyButton.image.sprite = easyDefaultSprite;
+            //normalButton.image.sprite = normalDefaultSprite;
+
+        }
     }
 
     public void Easy()
@@ -16,20 +55,32 @@ public class MainMenu : MonoBehaviour
         Difficulty.difficulty.easy = true;
         Difficulty.difficulty.normal = false;
         Difficulty.difficulty.hard = false;
+        normalButton.image.sprite = normalDefaultSprite;
+        hardButton.image.sprite = hardDefaultSprite;
+
+
     }
     public void Normal()
     {
         Difficulty.difficulty.easy = false;
         Difficulty.difficulty.normal = true;
         Difficulty.difficulty.hard = false;
+        easyButton.image.sprite = easyDefaultSprite;
+        hardButton.image.sprite = hardDefaultSprite;
+
 
     }
     public void Hard()
     {
         Difficulty.difficulty.easy = false;
-        Difficulty.difficulty.normal = false;
+        Difficulty.difficulty.normal = false;       
         Difficulty.difficulty.hard = true;
+        easyButton.image.sprite = easyDefaultSprite;
+        normalButton.image.sprite = normalDefaultSprite;
+
     }
+
+
 
 }
 
