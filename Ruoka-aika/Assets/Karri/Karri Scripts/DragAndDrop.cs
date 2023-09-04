f11808422f570528ecbc069d2843cf54437f2a3f
skillzz123jj <turpeinenmila6@gmail.com>
2023-09-05 00:18:23 +0300

    Bug fixes

--- Ruoka-aika/Assets/Karri/Karri Scripts/DragAndDrop.cs
+++ Ruoka-aika/Assets/Karri/Karri Scripts/DragAndDrop.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
     private bool isDragging;
-
+  
+    public static DragAndDrop dragAndDrop;
     public void OnMouseDown()
     {
-        isDragging = true;
+        isDragging = true;        
+
     }
 
     public void OnMouseUp()
@@ -20,10 +24,14 @@ public class DragAndDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isDragging)
+        if (isDragging)
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(mousePosition);
         }
     }
 }
+
+
+
+
