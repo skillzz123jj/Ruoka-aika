0aa9298e97d9bd3f26ab3e6c561f90c2ce6f2805
Karri Sivén <meatballgod89@gmail.com>
2023-09-26 10:31:46 +0300

    Update DragAndDrop.cs

--- Ruoka-aika/Assets/Karri/Karri Scripts/DragAndDrop.cs
+++ Ruoka-aika/Assets/Karri/Karri Scripts/DragAndDrop.cs
@@ -1,18 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DragAndDrop : MonoBehaviour
 {
-    private bool isDragging;
-  
-    public static DragAndDrop dragAndDrop;
+    public Dictionary<GameObject, Vector2> FoodPositionDictionary = new Dictionary<GameObject, Vector2>();
+
+    public float arrowKeysSpeed = 5.0f; // Adjust this speed as needed
+
+    private bool isDragging = false;
+    private Vector3 offset;
+
     public void OnMouseDown()
     {
-        isDragging = true;        
-
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     public void OnMouseUp()
@@ -20,18 +21,37 @@ public class DragAndDrop : MonoBehaviour
         isDragging = false;
     }
 
-
     // Update is called once per frame
     void Update()
     {
         if (isDragging)
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.Translate(mousePosition);
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePosition + offset;
+
+            // Update the position in the dictionary
+            if (FoodPositionDictionary.ContainsKey(gameObject))
+            {
+                FoodPositionDictionary[gameObject] = transform.position;
+            }
+        }
+        else
+        {
+            // Get input from the arrow keys only if not dragging with the mouse
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            // Calculate the movement direction
+            Vector2 moveDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+            // Apply the movement
+            transform.Translate(moveDirection * arrowKeysSpeed * Time.deltaTime);
+
+            // Update the position in the dictionary
+            if (FoodPositionDictionary.ContainsKey(gameObject))
+            {
+                FoodPositionDictionary[gameObject] = transform.position;
+            }
         }
     }
 }
-
-
-
-
